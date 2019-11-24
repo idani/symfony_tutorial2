cd91f9ca9d8af177b4efc2a8fc03bbc09938c331
Shigehiro IDANI <4g@idani.jp>
2019-11-25 00:21:43 +0900

    template練習

--- src/__symfony/src/Controller/BlogController.php
+++ src/__symfony/src/Controller/BlogController.php
@@ -8,12 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="blog_index")
+     *
+     * @return void
+     */
+    public function index()
+    {}
+
     /**
      * @Route("/blog/{page}", name="blog_list", requirements={"page"="\d+"})
      */
     public function list(int $page)
     {
-        return $this->render('lucky/number.html.twig', ['number' => __FILE__ . '(' . __LINE__ . '):' . __FUNCTION__ . ' page:' . $page]);
+        // return $this->render('lucky/number.html.twig', ['number' => __FILE__ . '(' . __LINE__ . '):' . __FUNCTION__ . ' page:' . $page]);
+        return $this->render('blog/list.html.twig', ['blog_posts' => [], 'articles' => []]);
     }
 
 
